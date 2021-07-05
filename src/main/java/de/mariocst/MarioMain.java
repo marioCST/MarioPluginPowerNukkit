98d0dce7515243b571b94c3df1c3af091b67bc96
marioCST <marioCST@noreply.github.com>
2021-07-05 11:42:32 +0200

    Removed unused Import

--- src/main/java/de/mariocst/MarioMain.java
+++ src/main/java/de/mariocst/MarioMain.java
@@ -34,7 +34,6 @@ import lombok.Getter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class MarioMain extends PluginBase implements AntiCheatAPI {
 
