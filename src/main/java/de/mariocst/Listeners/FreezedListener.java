43815e6afc838783df549b358a4a03a996959529
marioCST <marioCST@noreply.github.com>
2021-07-08 10:59:49 +0200

    Better Anti Hack, trying later a fix for No Lag back

--- src/main/java/de/mariocst/Listeners/FreezedListener.java
+++ src/main/java/de/mariocst/Listeners/FreezedListener.java
@@ -4,6 +4,7 @@ import cn.nukkit.Player;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerMoveEvent;
+import cn.nukkit.level.Location;
 import de.mariocst.MarioMain;
 
 public class FreezedListener implements Listener {
@@ -11,6 +12,7 @@ public class FreezedListener implements Listener {
     public void onFreeze(PlayerMoveEvent event) {
         for (Player freezed : MarioMain.getInstance().freezed) {
             if (event.getFrom().getX() != freezed.getX() || event.getFrom().getY() != freezed.getY() || event.getFrom().getZ() != freezed.getZ()) {
+                event.setTo(new Location(event.getFrom().getX(), event.getFrom().getY(), event.getFrom().getZ(), freezed.getPitch(), freezed.getYaw()));
                 event.setCancelled(true);
             }
         }
