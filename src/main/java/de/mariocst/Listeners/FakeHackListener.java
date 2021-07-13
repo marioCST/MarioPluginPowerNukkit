0fc519e0351d0fd7b30da560425b9022162747b6
marioCST <marioCST@noreply.github.com>
2021-07-13 20:45:00 +0200

    Removed unused Imports

--- src/main/java/de/mariocst/Listeners/FakeHackListener.java
+++ src/main/java/de/mariocst/Listeners/FakeHackListener.java
@@ -1,12 +1,9 @@
 package de.mariocst.Listeners;
 
 import cn.nukkit.Player;
-import cn.nukkit.entity.Entity;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
-import cn.nukkit.event.entity.EntityDamageEvent;
 import cn.nukkit.event.player.PlayerMoveEvent;
-import cn.nukkit.level.Location;
 import de.mariocst.MarioMain;
 
 public class FakeHackListener implements Listener {
